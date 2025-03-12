a0a105e78e60752cca902cda99ec542a14c6d184
Slouumaa <abdessalem.chaouch@esprit.tn>
2025-03-12 05:54:51 +0100

    slouma commit

--- Serveur/src/models/staff.js
+++ Serveur/src/models/staff.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const staffSchema = new mongoose.Schema({
   badgeNumber: { type: Number, required: true, unique: true },
-  function: { type: String, required: true }, // ex: nurse, doctor, etc.
+  service: { type: String, required: true }, // ex: nurse, doctor, etc.
    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 }, { timestamps: false, versionKey: false });
 
