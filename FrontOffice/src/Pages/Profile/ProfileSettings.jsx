6054d53e8d6d81f8004b740fba720bd35fc607bc
YOSRcharek <yosryassoura31@gmail.com>
2025-04-27 15:09:43 +0100

    profile

--- FrontOffice/src/Pages/Profile/ProfileSettings.jsx
+++ FrontOffice/src/Pages/Profile/ProfileSettings.jsx
@@ -1,120 +1,126 @@
-import React from 'react';
-import './style.css';
+import { useState ,React,useRef} from 'react';
+import { FaArrowRight, FaCog, FaInfoCircle, FaStethoscope, FaNotesMedical } from 'react-icons/fa'; // Remplacer FaArrowRightLong par FaArrowRight
+
 const ProfileSettings = () => {
-    return (
-        <div className="container light-style flex-grow-1 container-p-y">
-            <h4 className="font-weight-bold py-3 mb-4">Account settings</h4>
-            <div className="card overflow-hidden">
-                <div className="row no-gutters row-bordered row-border-light">
-                    <div className="col-md-3 pt-0">
-                        <div className="list-group list-group-flush account-settings-links">
-                            <a className="list-group-item list-group-item-action active" data-toggle="list" href="#account-general">General</a>
-                            <a className="list-group-item list-group-item-action" data-toggle="list" href="#account-change-password">Change password</a>
-                            <a className="list-group-item list-group-item-action" data-toggle="list" href="#account-info">Info</a>
-                       </div>
-                    </div>
-                    <div className="col-md-9">
-                        <div className="tab-content">
-                            {/* General Tab */}
-                            <div className="tab-pane fade active show" id="account-general">
-                                <div className="card-body media align-items-center">
-                                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="User Avatar" className="d-block ui-w-80" />
-                                    <div className="media-body ml-4">
-                                        <label className="btn btn-outline-primary">
-                                            Upload new photo
-                                            <input type="file" className="account-settings-fileinput" />
-                                        </label> &nbsp;
-                                        <button type="button" className="btn btn-default md-btn-flat">Reset</button>
-                                        <div className="text-light small mt-1">Allowed JPG, GIF or PNG. Max size of 800K</div>
-                                    </div>
-                                </div>
-                                <hr className="border-light m-0" />
-                                <div className="card-body">
-                                    <div className="form-group">
-                                        <label className="form-label">Username</label>
-                                        <input type="text" className="form-control mb-1" defaultValue="nmaxwell" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Name</label>
-                                        <input type="text" className="form-control" defaultValue="Nelle Maxwell" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">E-mail</label>
-                                        <input type="text" className="form-control mb-1" defaultValue="nmaxwell@mail.com" />
-                                        <div className="alert alert-warning mt-3">
-                                            Your email is not confirmed. Please check your inbox.<br />
-                                            <a href="javascript:void(0)">Resend confirmation</a>
-                                        </div>
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Company</label>
-                                        <input type="text" className="form-control" defaultValue="Company Ltd." />
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* Change Password Tab */}
-                            <div className="tab-pane fade" id="account-change-password">
-                                <div className="card-body pb-2">
-                                    <div className="form-group">
-                                        <label className="form-label">Current password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">New password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Repeat new password</label>
-                                        <input type="password" className="form-control" />
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* Info Tab */}
-                            <div className="tab-pane fade" id="account-info">
-                                <div className="card-body pb-2">
-                                    <div className="form-group">
-                                        <label className="form-label">Bio</label>
-                                        <textarea className="form-control" rows="5">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</textarea>
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Birthday</label>
-                                        <input type="text" className="form-control" defaultValue="May 3, 1995" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Country</label>
-                                        <select className="custom-select">
-                                            <option>USA</option>
-                                            <option selected>Canada</option>
-                                            <option>UK</option>
-                                            <option>Germany</option>
-                                            <option>France</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <hr className="border-light m-0" />
-                                <div className="card-body pb-2">
-                                    <h6 className="mb-4">Contacts</h6>
-                                    <div className="form-group">
-                                        <label className="form-label">Phone</label>
-                                        <input type="text" className="form-control" defaultValue="+0 (123) 456 7891" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="form-label">Website</label>
-                                        <input type="text" className="form-control" />
-                                    </div>
-                                </div>
-                            </div>
-
-                         
-                         
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    const [activeTab, setActiveTab] = useState('general'); // Onglet par défaut
+   
+     const handleTabClick = (tab, ref) => {
+       setActiveTab(tab); // Mettre à jour l'onglet actif
+       ref.current.scrollIntoView({ behavior: 'smooth' }); // Faire défiler vers la section de l'onglet sélectionné
+     };
+   
+     // Références pour chaque section
+     const generalRef = useRef(null);
+     const infoRef = useRef(null);
+     const consultationsRef = useRef(null);
+     const medicalRecordRef = useRef(null);
+   
+     return (
+       <>
+         <section className="py-[120px] bg-BodyBg-0">
+           <div className="Container">
+             <div className="grid grid-cols-3 gap-[50px] lg:gap-8 xl:gap-[50px]">
+               <div className="col-span-3 lg:col-span-1">
+                 <div className="rounded-2xl px-7 pt-7 pb-6 overflow-hidden bg-white bg-opacity-30 border-2 border-white border-opacity-80 mb-7">
+                   <h4 className="font-AlbertSans font-semibold text-2xl text-HeadingColor-0 pb-2 mb-8 relative before:absolute before:bottom-0 before:left-0 before:w-7 before:h-[2px] before:bg-PrimaryColor-0">
+                     Account Settings
+                   </h4>
+                   <ul className="mt-8">
+                     <li>
+                       <button
+                         onClick={() => handleTabClick('general', generalRef)}
+                         className={`w-full font-AlbertSans text-left text-HeadingColor-0 transition-all duration-500 group px-7 py-4 flex items-center justify-between rounded-md bg-white bg-opacity-30 border-2 border-white border-opacity-80 mb-3 overflow-hidden ${
+                           activeTab === 'general'
+                             ? 'bg-PrimaryColor-0 text-white'
+                             : 'hover:bg-PrimaryColor-0 hover:text-white'
+                         }`}
+                       >
+                         <span className="flex items-center gap-3 lg:gap-1 xl:gap-3">
+                           <FaCog className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                           General
+                         </span>
+                         <FaArrowRight className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         onClick={() => handleTabClick('info', infoRef)}
+                         className={`w-full font-AlbertSans text-left text-HeadingColor-0 transition-all duration-500 group px-7 py-4 flex items-center justify-between rounded-md bg-white bg-opacity-30 border-2 border-white border-opacity-80 mb-3 overflow-hidden ${
+                           activeTab === 'info'
+                             ? 'bg-PrimaryColor-0 text-white'
+                             : 'hover:bg-PrimaryColor-0 hover:text-white'
+                         }`}
+                       >
+                         <span className="flex items-center gap-3 lg:gap-1 xl:gap-3">
+                           <FaInfoCircle className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                           Info
+                         </span>
+                         <FaArrowRight className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         onClick={() => handleTabClick('consultations', consultationsRef)}
+                         className={`w-full font-AlbertSans text-left text-HeadingColor-0 transition-all duration-500 group px-7 py-4 flex items-center justify-between rounded-md bg-white bg-opacity-30 border-2 border-white border-opacity-80 mb-3 overflow-hidden ${
+                           activeTab === 'consultations'
+                             ? 'bg-PrimaryColor-0 text-white'
+                             : 'hover:bg-PrimaryColor-0 hover:text-white'
+                         }`}
+                       >
+                         <span className="flex items-center gap-3 lg:gap-1 xl:gap-3">
+                           <FaStethoscope className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                           Consultations
+                         </span>
+                         <FaArrowRight className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         onClick={() => handleTabClick('medicalRecord', medicalRecordRef)}
+                         className={`w-full font-AlbertSans text-left text-HeadingColor-0 transition-all duration-500 group px-7 py-4 flex items-center justify-between rounded-md bg-white bg-opacity-30 border-2 border-white border-opacity-80 mb-3 overflow-hidden ${
+                           activeTab === 'medicalRecord'
+                             ? 'bg-PrimaryColor-0 text-white'
+                             : 'hover:bg-PrimaryColor-0 hover:text-white'
+                         }`}
+                       >
+                         <span className="flex items-center gap-3 lg:gap-1 xl:gap-3">
+                           <FaNotesMedical className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                           Medical Record
+                         </span>
+                         <FaArrowRight className="text-PrimaryColor-0 transition-all duration-500 group-hover:text-white" />
+                       </button>
+                     </li>
+                   </ul>
+                 </div>
+               </div>
+   
+               {/* Section correspondant à l'onglet sélectionné */}
+               <div className="col-span-3 lg:col-span-2">
+                 <div ref={generalRef} id="general" className={`tab-section ${activeTab === 'general' ? 'block' : 'hidden'}`}>
+                   <h2 className="font-AlbertSans font-bold text-[28px] text-HeadingColor-0 mb-10">General</h2>
+                   {/* Contenu pour l'onglet General */}
+                 </div>
+   
+                 <div ref={infoRef} id="info" className={`tab-section ${activeTab === 'info' ? 'block' : 'hidden'}`}>
+                   <h2 className="font-AlbertSans font-bold text-[28px] text-HeadingColor-0 mb-10">Info</h2>
+                   {/* Contenu pour l'onglet Info */}
+                 </div>
+   
+                 <div ref={consultationsRef} id="consultations" className={`tab-section ${activeTab === 'consultations' ? 'block' : 'hidden'}`}>
+                   <h2 className="font-AlbertSans font-bold text-[28px] text-HeadingColor-0 mb-10">Consultations</h2>
+                   {/* Contenu pour l'onglet Consultations */}
+                 </div>
+   
+                 <div ref={medicalRecordRef} id="medicalRecord" className={`tab-section ${activeTab === 'medicalRecord' ? 'block' : 'hidden'}`}>
+                   <h2 className="font-AlbertSans font-bold text-[28px] text-HeadingColor-0 mb-10">Medical Record</h2>
+                   {/* Contenu pour l'onglet Medical Record */}
+                 </div>
+               </div>
+             </div>
+           </div>
+         </section>
+       </>
+     );
 };
 
 export default ProfileSettings;
